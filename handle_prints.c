8d1ecf67bd355650e063cc1dba771c40d4c8e9bc
danstan <oduordanstan@gmail.com>
2023-04-25 22:26:41 +0300

    returned the null value to its correct position

--- handle_prints.c
+++ handle_prints.c
@@ -16,11 +16,11 @@ int flags, int width, int precision, int size)
 {
 int i, unknow_len = 0, printed_chars = -1;
 fmt_t fmt_types[] = {
-{'c', print_char}, {'s', print_string}, {'%', print_percent}, {'\0', NULL},
+{'c', print_char}, {'s', print_string}, {'%', print_percent},
 {'i', print_int}, {'d', print_int}, {'b', print_binary},
 {'u', print_unsign}, {'o', print_oct}, {'x', print_hex_dec},
 {'X', print_hex_upper}, {'p', print_pointer}, {'S', non_printable},
-{'r', print_reverse}, {'R', print_rot13str}
+{'r', print_reverse}, {'R', print_rot13str}, {'\0', NULL}
 };
 for (i = 0; fmt_types[i].fmt != '\0'; i++)
 if (fmt[*ind] == fmt_types[i].fmt)
